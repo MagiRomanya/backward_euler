Fixed differentables in np.gradient

differentiable/main2.py
@@ -42,43 +42,37 @@ def simulate(k, k_bend):
         ##################################
         # Newton Iterations
         ##################################
-        iterations = 2
+        iterations = 3
         xi = x
         vi = v
         for it in range(iterations):
             xi, vi = newton_iteration(sim, x, v, xi, vi)
             sim.set_state(xi, vi)
             sim.fill_containers()
-            # delta_v = vi - v
-            # f = symulathon.get_force()
-            # print(f"{it+1}th iteration convergence check = {sum(mass @ delta_v - h * f)}")
 
     return backpropagation
 
 
-# def calc_gradient(surface: np.array):
-#     dgdx = np.zeros(surface.shape)
-#     dgdy = np.zeros(surface.shape)
-#     x_dim, y_dim = surface.shape
-#     for i in range(x_dim):
-#         for j in range(y_dim):
-#             dgdx =
-
 if __name__ == "__main__":
+    # Initialize important constant variables
     sim = Simulation([1, 1])
     nDoF = sim.getDoF()
     mass = sim.getMassMatrix()
     h = sim.getTimeStep()
     DIFF_FRAMES = 10
 
-    n_points = 30
+    # Define the region studied
+    n_points = 20
     k_values = np.linspace(0.01, 10, n_points)
     k_bend_values = np.linspace(-10, 500, n_points-1) / 100
     X, Y = np.meshgrid(k_values, k_bend_values)
+
     g_values = X.tolist()
     dgdk_values = X.tolist()
     dgdk_bend_values = X.tolist()
     ones = np.zeros(X.shape)
+
+    # Computation of the surface and gradients
     for i in tqdm(range(len(k_values))):
         for j in range(len(k_bend_values)):
             bp = simulate(k_values[i], k_bend_values[j])
@@ -87,19 +81,25 @@ if __name__ == "__main__":
             dgdk_bend_values[j][i] = dgdp[1]
             g_values[j][i] = bp.get_g()
 
+    # Finite differences
+    dk = k_values[1]-k_values[0]
+    dk_bend = k_bend_values[1]-k_bend_values[0]
+    # REVIEW: Why does this gradient function accept first dy and then dx and
+    # also returns first dfdy and then dfdx
+    dgdk_bend_values_finite, dgdk_values_finite = np.gradient(g_values, dk_bend, dk)
+
+    # Plotting
+    # 3D plot
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     g_values = np.array(g_values)
     dgdk_values = np.array(dgdk_values)
     dgdk_bend_values = np.array(dgdk_bend_values)
-    dk = k_values[1]-k_values[0]
-    dk_bend = k_bend_values[1]-k_bend_values[0]
-    dgdk_bend_values_finite, dgdk_values_finite = np.gradient(g_values, dk, dk_bend)
     diff = np.sqrt(
         (dgdk_values - dgdk_values_finite)**2 +
         (dgdk_bend_values - dgdk_bend_values_finite)**2)
     dfdp_magnitude_finite = np.sqrt(
         dgdk_values_finite**2 + dgdk_bend_values_finite**2)
-    diff_rel = diff / dfdp_magnitude_finite
+    diff_rel = diff / np.max(dfdp_magnitude_finite)
 
     colors = cm.Greys(diff)
     surf = ax.plot_surface(X, Y, g_values, facecolors=colors)
@@ -126,6 +126,12 @@ if __name__ == "__main__":
     # fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
     plt.contourf(X, Y, g_values, levels=40)
+    plt.colorbar()
+    plt.quiver(X, Y, dgdk_values, dgdk_bend_values, color="blue")
+    plt.quiver(X, Y, dgdk_values_finite, dgdk_bend_values_finite , color="red")
+    plt.show()
+
+    plt.contourf(X, Y, diff_rel, levels=40)
     plt.colorbar()
     plt.quiver(X, Y, dgdk_values, dgdk_bend_values, color="blue")
     plt.quiver(X, Y, dgdk_values_finite, dgdk_bend_values_finite , color="red")